a53b9ef60e8166710965951eb74b786a3a8b00b5
nexus4812 <powerbatteryboy@gmail.com>
2021-06-24 14:13:19 +0900

    Add demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Link, BrowserRouter as Router, useLocation} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <QueryParamsDemo />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function createTo(query:URLSearchParams, name: string, value: string):string {
+    if(query.has(name)) {
+        query.delete(name);
+    }
+
+    query.append(name, value);
+    return "?"+query.toString();
+}
+
+function QueryParamsDemo() {
+    const [httpbinResponse, setHttpbinResponse] = useState<any>({url : ''});
+
+    const location = useLocation();
+
+    const query = new URLSearchParams(location.search);
+
+    useEffect( () => {
+        const f = async () => {
+            const q = new URLSearchParams(location.search);
+            const response = await fetch(`https://httpbin.org/get?${q.toString()}`)
+            const date = await response.json();
+            setHttpbinResponse(date);
+        };
+
+        f();
+    }, [location.search]);
+
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to={createTo(query, 'name', "taro")}>Add Name taro</Link>
+                </li>
+                <li>
+                    <Link to={createTo(query, 'name', "kuro")}>Add Name kuro</Link>
+                </li>
+                <li>
+                    <Link to={createTo(query, 'site', "google")}>Add site google</Link>
+                </li>
+                <li>
+                    <Link to={createTo(query, 'site', "yahoo")}>Add site yahoo</Link>
+                </li>
+            </ul>
+
+            <div>
+                {httpbinResponse.url}
+            </div>
+        </>
+    );
+}
